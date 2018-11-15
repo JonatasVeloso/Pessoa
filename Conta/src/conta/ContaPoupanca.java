2f85b100d5c9570a15a136434a07ffd8053cfa60
Jonatas Veloso <jntsvls@gmail.com>
2018-11-15 13:55:58 -0200

    Deletando Conta e Adicionando o Porjeto Pessoa

--- Conta/src/conta/ContaPoupanca.java
+++
@@ -1,8 +0,0 @@
-package conta;
-
-public final class ContaPoupanca extends Conta{
-	public ContaPoupanca(int agencia, int numero) {
-		super(agencia, numero);
-	}
-
-}
---
+++ src/Funcionario.java
@@ -0,0 +1,24 @@
+
+public abstract class Funcionario {
+	private String nome;
+	private String cpf;
+	private double salario;
+	
+	public abstract void getBonificaacao();
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
+}
